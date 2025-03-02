1655d579cd36b010fab920153dc316a9d065f204
Junhyok <cjh64866@naver.com>
2025-03-02 21:51:07 +0900

    DESIGN: BottomBtn 색상 조건 설정

--- src/components/button/BottomBtn.tsx
+++ src/components/button/BottomBtn.tsx
@@ -2,9 +2,14 @@ import { Button, ButtonProps } from "@mui/material";
 
 import "./BottomBtn.css";
 
-const BottomBtn: React.FC<ButtonProps> = ({ children, ...props }) => {
+const BottomBtn: React.FC<ButtonProps> = ({ children, variant, ...props }) => {
   return (
-    <Button className="__bottom-btn__" sx={{ background: "#007bff" }} {...props}>
+    <Button
+      className="__bottom-btn__"
+      variant={variant}
+      sx={variant === "contained" ? { background: "#007bff" } : {}}
+      {...props}
+    >
       {children}
     </Button>
   );
