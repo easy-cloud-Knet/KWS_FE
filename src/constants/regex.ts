74a60073f1227d1b7c0feb94b060123f1111f84b
Choi JunHyeok <cjh64866@naver.com>
2025-03-17 17:48:04 +0900

    FIX: auth regex 재수정

--- src/constants/regex.ts
+++ src/constants/regex.ts
@@ -1,3 +1,3 @@
-export const EMAIL_REGEX = /^[0-9._%+-]+@kw\.ac\.kr$/;
+export const EMAIL_REGEX = /^[A-Za-z0-9._%+-]+@kw\.ac\.kr$/;
 export const PW_REGEX = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,100}$/;
 export const NAME_REGEX = /^[A-Za-z0-9ㄱ-ㅎ가-힣]{2,12}$/;
