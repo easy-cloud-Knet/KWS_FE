f0a6c036b7897f4a3061f2becf374d4e5f451206
Junhyeok <cjh64866@naver.com>
2025-03-25 01:52:01 +0900

    FIX: 회원가입 시 간헐적으로 username 비어있던 문제

--- src/pages/auth/signUp/SignUp3.tsx
+++ src/pages/auth/signUp/SignUp3.tsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import AuthTextFieldV2 from "../../../components/auth/textField/AuthTextFieldV2";
 import BottomBtn from "../../../components/button/BottomBtn";
+
+import axiosClient from "../../../services/api";
 
 import { UserInfo } from "../../../types/auth";
 
@@ -10,18 +13,20 @@ import { NAME_REGEX } from "../../../constants/regex";
 interface SignUp3Props {
   userInfo: UserInfo;
   setUserInfo: React.Dispatch<React.SetStateAction<UserInfo>>;
-  onClickRegisterAllowButton: () => void;
 }
 
 /**
  * @param {function} onClickRegisterAllowButton - 회원가입 완료 버튼 클릭 시 호출되는 함수
  */
-const SignUp3: React.FC<SignUp3Props> = ({ userInfo, setUserInfo, onClickRegisterAllowButton }) => {
+const SignUp3: React.FC<SignUp3Props> = ({ userInfo, setUserInfo }) => {
   const [name, setName] = useState(userInfo.name);
   const [nameChecker, setNameChecker] = useState({
     show: false,
     format: false,
   });
+
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     setNameChecker((prevNameChecker) => ({
@@ -29,6 +34,30 @@ const SignUp3: React.FC<SignUp3Props> = ({ userInfo, setUserInfo, onClickRegiste
       format: NAME_REGEX.test(name),
     }));
   }, [name]);
+
+  // 회원가입 완료 버튼 클릭 시
+  const onClickRegisterAllowButton = async () => {
+    setIsLoading(true);
+    try {
+      await axiosClient.post("/users/register", {
+        email: userInfo.email,
+        password: userInfo.pw,
+        username: name || userInfo.name,
+      });
+
+      navigate("/signup/success");
+    } catch (error) {
+      const err = error as { response?: { data: { detail?: Array<{ msg: string }> } } };
+      alert(err.response?.data.detail || "회원가입 실패");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  if (isLoading) {
+    // TODO: 로딩 추가
+    return <div>회원가입 중...</div>;
+  }
 
   return (
     <div>
--- src/pages/auth/signUp/SignUpDefault.tsx
+++ src/pages/auth/signUp/SignUpDefault.tsx
@@ -1,13 +1,10 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 
 import SignUp1 from "./SignUp1";
 import SignUp2 from "./SignUp2";
 import SignUp3 from "./SignUp3";
 import SignUp4 from "./SignUpSuccess";
 import Selector from "../../../components/auth/Selector";
-
-import axiosClient from "../../../services/api";
 
 import { UserInfo } from "../../../types/auth";
 
@@ -24,11 +21,6 @@ const SignUpDefault = () => {
     name: "",
   });
 
-  // loading state
-  const [isLoading, setIsLoading] = useState(false);
-
-  const navigate = useNavigate();
-
   // 이전 단계
   // const onPrevious = () => {
   //   setStep(step - 1);
@@ -38,30 +30,6 @@ const SignUpDefault = () => {
     setStep(step + 1);
   };
 
-  // 회원가입 완료 버튼 클릭 시
-  const onClickRegisterAllowButton = async () => {
-    setIsLoading(true);
-    try {
-      await axiosClient.post("/users/register", {
-        email: userInfo.email,
-        password: userInfo.pw,
-        username: userInfo.name,
-      });
-      console.log(userInfo);
-      navigate("/signup/success");
-    } catch (error) {
-      const err = error as { response?: { data: { detail?: Array<{ msg: string }> } } };
-      alert(err.response?.data.detail || "회원가입 실패");
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  if (isLoading) {
-    // TODO: 로딩 중일 때 보여줄 화면
-    return <div>로딩중...</div>;
-  }
-
   return (
     <div className="signup-default j-content-center">
       <div className="signup-container">
@@ -69,13 +37,7 @@ const SignUpDefault = () => {
         <h2 className="title p-36-400 t-align-center">회원가입</h2>
         {step === 1 && <SignUp1 onNext={onNext} userInfo={userInfo} setUserInfo={setUserInfo} />}
         {step === 2 && <SignUp2 onNext={onNext} userInfo={userInfo} setUserInfo={setUserInfo} />}
-        {step === 3 && (
-          <SignUp3
-            userInfo={userInfo}
-            setUserInfo={setUserInfo}
-            onClickRegisterAllowButton={onClickRegisterAllowButton}
-          />
-        )}
+        {step === 3 && <SignUp3 userInfo={userInfo} setUserInfo={setUserInfo} />}
         {step === 4 && <SignUp4 />}
       </div>
     </div>
