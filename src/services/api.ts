5b8295b094bd6ffa0ed73b1e97a5ed311338b7b6
Junhyeok <cjh64866@naver.com>
2025-06-23 18:08:29 +0900

    feat: axiosClient 인터셉터 수정

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,10 +5,20 @@ const axiosClient = axios.create({
   baseURL: "http://100.101.247.128:25121/api",
   headers: {
     "Content-Type": "application/json",
-    Authorization: Cookies.get("accessToken")
-      ? `Bearer ${Cookies.get("accessToken")}`
-      : undefined,
   },
 });
+
+axiosClient.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("accessToken") || "";
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosClient;
