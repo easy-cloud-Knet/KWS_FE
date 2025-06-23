088497ecada6aa8ef8c4e94de6a33d77b4d1dfc1
Junhyeok <cjh64866@naver.com>
2025-06-23 17:17:02 +0900

    feat: 서버 URL 변경

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,10 +2,12 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosClient = axios.create({
-  baseURL: "http://223.194.20.119:25121/api",
+  baseURL: "http://kws-control:25121/api",
   headers: {
     "Content-Type": "application/json",
-    Authorization: Cookies.get("accessToken") ? `Bearer ${Cookies.get("accessToken")}` : undefined,
+    Authorization: Cookies.get("accessToken")
+      ? `Bearer ${Cookies.get("accessToken")}`
+      : undefined,
   },
 });
 
