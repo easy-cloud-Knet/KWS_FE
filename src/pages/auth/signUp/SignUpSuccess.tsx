a2c9648d72c6bb4c3896c6220e7c8cb6a903fbc5
Junhyeok <cjh64866@naver.com>
2025-09-01 13:55:09 +0900

    feat: SignUpSuccess 페이지 figma 수정사항 반영

--- src/pages/auth/signUp/SignUpSuccess.tsx
+++ src/pages/auth/signUp/SignUpSuccess.tsx
@@ -1,31 +1,36 @@
 import { useNavigate } from "react-router-dom";
 
-import BottomBtn from "../../../components/button/BottomBtn";
-
-import "./../AuthDefault.css";
-import "./SignUpDefault.css";
-import "./SignUpSuccess.css";
+import ic_check_auth from "@/assets/image/auth/ic_check_auth.svg";
+import BottomBtn from "@/components/button/BottomBtn";
 
 const SignUpSuccess = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="signup-success signup-default">
-      <div className="signup-container">
-        <h2 className="p-48-400 t-align-center">회원가입 완료</h2>
+    <main className="flex flex-col items-center pt-[216px] w-full">
+      <div className="flex flex-col items-center gap-[32px]">
+        <img src={ic_check_auth} alt="" />
 
-        <p className="p-20-400 t-align-center c-grey">KWS에 오신 것을 환영합니다!</p>
+        <h2 className="text-pr-sb-36 text-center">회원가입 완료</h2>
 
-        <div className="bottom-btn-wrap f-dir-column">
-          <BottomBtn variant="outlined" onClick={() => navigate("/")}>
-            메인 화면으로
-          </BottomBtn>
-          <BottomBtn variant="contained" onClick={() => navigate("/signin")}>
-            로그인
-          </BottomBtn>
-        </div>
+        <p className="text-pr-r-21 text-center c-grey">
+          도들에 오신 것을 환영합니다. <br />
+          이제부터 클라우드 환경을 자유롭게 탐험해 보세요!
+        </p>
       </div>
-    </div>
+
+      <div className="flex flex-col items-center gap-[32px] mt-[99px] mb-[32px] w-[452px]">
+        <BottomBtn variant="contained" onClick={() => navigate("/signin")}>
+          로그인
+        </BottomBtn>
+        <button
+          className="text-(--Main_Blue) text-pr-r-18 text-center underline underline-offset-8 cursor-pointer"
+          onClick={() => navigate("/")}
+        >
+          메인 화면으로
+        </button>
+      </div>
+    </main>
   );
 };
 
