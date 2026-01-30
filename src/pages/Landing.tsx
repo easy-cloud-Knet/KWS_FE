5dd46c643c0672edf0668cf17fbdd52e18d0f9ed
seooo-h <happy051024@naver.com>
2026-01-31 01:33:37 +0900

    feat: landinpage변경

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,31 +1,29 @@
 import { useNavigate } from "react-router-dom";
-
-import connectorLine from "@/assets/image/landing/connector_line.svg";
 import horizontalArrow from "@/assets/image/landing/horizontal_arrow.svg";
 import ic_baseline_copyright from "@/assets/image/landing/ic_baseline_copyright.svg";
-import rectangle from "@/assets/image/landing/rectangle.svg";
 import LandingInputField from "@/components/LandingInputField";
 
 const Landing = () => {
   const navigate = useNavigate();
   return (
     <main className="w-full max-w-[1200px] mx-auto">
-      <section className="flex justify-between pt-[169px] w-full h-[calc(100vh-103px)]">
+      <section className="flex justify-center items-center w-full h-[calc(100vh-290px)]">
         <div className="flex flex-col justify-between pt-[82px] pb-[68px] h-[502px]">
-          <h1 className="text-(--BlueBlack) text-[40px] font-semibold">
+          <h1 className="text-(--BlueBlack) text-[96px] font-semibold text-center">
             당신의 <span className="text-(--Main_Blue)">아이디어</span>가
             실현되는
             <br />
             가장 빠른 길.
           </h1>
           <div className="flex flex-col gap-[32px]">
-            <p className="text-(--BlueBlack) text-[20px] font-normal">
+            <p className="text-(--BlueBlack) text-[20px] font-normal text-center mt-[25px]">
               생각하는 대로, 번거로움 없이. 바로 만드는 컴퓨팅 환경.
               <br />
               도들에서 당신의 가상 머신을 만들어 보세요.
             </p>
             <button
-              className="w-full h-[48px] bg-[var(--Main_Blue)] rounded-[10px] text-white font-semibold cursor-pointer"
+              className="block mx-auto w-[452px] h-[48px] bg-[var(--Main_Blue)] rounded-[10px] 
+                         text-white font-semibold cursor-pointer mt-[50px]"
               onClick={() => {
                 navigate("/manage");
               }}
@@ -34,53 +32,111 @@ const Landing = () => {
             </button>
           </div>
         </div>
-        <div className="w-[486px] h-[502px] bg-[#D9D9D9]"></div>
       </section>
       <section className="flex flex-col items-center pt-[272px] pb-[322px]">
-        <div className="w-[714px]">
-          <div className="flex justify-end">
-            <div className="flex flex-col gap-[56px] pt-[65px]">
-              <h1 className="text-[40px] font-semibold whitespace-nowrap">
+        <h1 className="text-[64px] font-semibold whitespace-nowrap text-center mb-[100px]">
+          어디서든, 클릭 단 세 번으로 생성
+        </h1>
+
+        <div className="w-full flex justify-center">
+          <div className="flex gap-[80px] justify-start">
+            <div
+              className="w-[448px] h-[403px] bg-white rounded-[20px]
+                      shadow-[0_0_24px_10px_#007BFF12]
+                      p-[32px]"
+            >
+              <p className="text-[40px] font-semibold whitespace-nowrap pb-[25px]">
                 손쉬운 <span className="text-(--Main_Blue)">인스턴스 생성</span>
-              </h1>
-              <p className="text-[20px] font-normal">
-                <strong>클릭 단 세 번</strong>으로
-                <br />
-                원하는 성능의 인스턴스 생성
+              </p>
+              <p className="text-[24px] font-semibold text-(--Grey3)">
+                클릭 단 세 번{" "}
+                <span className="font-normal">
+                  으로
+                  <br /> 원하는 성능의 인스턴스 생성
+                </span>
               </p>
             </div>
-            <img className="translate-x-1/2" src={rectangle} alt="" />
-          </div>
-          <img src={connectorLine} alt="" />
-          <div className="flex justify-start">
-            <img className="-translate-x-1/2" src={rectangle} alt="" />
-            <div className="flex flex-col gap-[56px] pt-[65px]">
-              <h1 className="text-[40px] font-semibold whitespace-nowrap">
-                <span className="text-(--Main_Blue)">웹 콘솔</span>
-              </h1>
-              <p className="text-[20px] font-normal whitespace-nowrap">
+
+            <div
+              className="w-[448px] min-h-[403px] bg-white rounded-[20px]
+                      shadow-[0_0_24px_10px_#007BFF12]
+                      p-[32px]"
+            >
+              <p className="text-[40px] font-semibold whitespace-nowrap pb-[25px]">
+                간편한 <span className="text-(--Main_Blue)">웹 콘솔</span>
+              </p>
+              <p className="text-[24px] font-normal text-(--Grey3)">
                 별다른 환경설정 없이도
                 <br />
-                <strong>어느 컴퓨터에서나</strong> 인스턴스에 접속
+                <span className="font-semibold">어느 컴퓨터에서나 </span>{" "}
+                인스턴스에 접속
               </p>
             </div>
-          </div>
-          <img className="scale-x-[-1]" src={connectorLine} alt="" />
-          <div className="flex justify-end">
-            <div className="flex flex-col gap-[56px] pt-[65px]">
-              <h1 className="text-[40px] font-semibold whitespace-nowrap">
+
+            <div
+              className="w-[448px] min-h-[403px] bg-white rounded-[20px]
+                      shadow-[0_0_24px_10px_#007BFF12]
+                      p-[32px]"
+            >
+              <p className="text-[40px] font-semibold whitespace-nowrap pb-[25px]">
                 인스턴스 생성 <span className="text-(--Main_Blue)">자동화</span>
-              </h1>
-              <p className="text-[20px] font-normal whitespace-nowrap">
+              </p>
+              <p className="text-[24px] font-normal text-(--Grey3)">
                 수많은 인스턴스 생성-삭제를
                 <br />
-                <strong>완전히 자동화</strong>
+                <span className="font-semibold"> 완전히 자동화</span>
               </p>
             </div>
-            <img className="translate-x-1/2" src={rectangle} alt="" />
           </div>
         </div>
       </section>
+
+      <section className="flex flex-col items-center pt-[272px] pb-[322px]">
+        <div className="absolute top-[2064px] left-[-170px] w-[2261px] h-[1272px] bg-[radial-gradient(ellipse_at_center,rgba(0,123,255,1),transparent_30%)] blur-[500px] pointer-events-none z-10" />
+        <div className="relative z-50">
+          <div className="w-full flex justify-center">
+            <div className="flex justify-start">
+              <div className="w-[1520px] h-[673px] grid grid-cols-[656px_426px_1fr] grid-rows-[310px_1fr] gap-[90px]">
+                {/* 왼쪽 큰 카드 */}
+                <div
+                  className="col-span-1 row-span-2 w-full h-full  bg-white rounded-[20px]
+                          shadow-[0_0_24px_10px_#007BFF12]
+                          p-[32px]"
+                >
+                  <p className="text-[48px] font-semibold whitespace-nowrap pb-[25px]">
+                    브라우저로{" "}
+                    <span className="text-(--Main_Blue)">즉시 관리</span>
+                  </p>
+                  <p className="text-[32px] font-normal text-(--Grey3)">
+                    언제 어디서나 웹 콘솔을 통해 인스턴스의
+                    <br /> 상태를 확인하고 원격 제어
+                  </p>
+                </div>
+
+                {/* 오른쪽 위 가로 카드 */}
+                <div
+                  className="col-span-2 w-full h-full  bg-white rounded-[20px]
+                          shadow-[0_0_24px_10px_#007BFF12]
+                          p-[32px]"
+                ></div>
+
+                {/* 오른쪽 아래 작은 카드들 */}
+                <div className="col-span-2 grid grid-cols-[2fr_1fr] gap-[80px]">
+                  <div
+                    className="h-full bg-white rounded-[20px] shadow-[0_0_24px_10px_#007BFF12]
+                          p-[32px]"
+                  />
+                  <div
+                    className="h-full bg-white rounded-[20px] shadow-[0_0_24px_10px_#007BFF12]
+                          p-[32px]"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
       <section className="flex flex-col items-center h-[1080px] pt-[403px]">
         <h1 className="mb-[56px] text-[40px] font-semibold">
           🔽 <span className="text-(--Main_Blue)">사전신청</span> 알림받기 🔽
