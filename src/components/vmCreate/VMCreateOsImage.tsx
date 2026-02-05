d2b7564d2b8787d3243236026742b56aa771e4a0
seooo-h <happy051024@naver.com>
2026-02-06 00:06:35 +0900

    fix: 버전선택 수정

--- src/components/vmCreate/VMCreateOsImage.tsx
+++ src/components/vmCreate/VMCreateOsImage.tsx
@@ -20,14 +20,14 @@ const VMCreateOsImage = ({ item }: VMCreateOsImageProps) => {
     <div
       key={item.name}
       className={twJoin(
-        "border-[1px]  rounded-[10px] w-[160px] h-[151px] flex-col items-center z-10",
+        "border-[1px]  rounded-[10px] w-[160px] h-[151px] flex-col items-center ",
         isSelected ? "border-main-blue" : "border-line",
       )}
     >
       <img src={item.img} alt={item.name} className="h-[48px] mt-[20px]" />
       <p className="mt-[8px] mb-[16px] typo-pr-m-16">{item.name}</p>
       <hr className="w-full border-line" />
-      <div className="relative z-[9999]">
+      <div className="absolute z-[9999] top-full left-0">
         <VersionDropdown
           item={item}
           osVersion={isSelected ? osVersion : ""}
